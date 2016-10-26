d2ebe8e860bf63a4d605d800d7734e1aba9ecca8
aw9223 <fukeric@gmail.com>
2016-10-26 11:43:29 +0900

    리소스 내려받기 수정

--- src/main/java/com/github/yjcpaj4/play_with_us/Main.java
+++ src/main/java/com/github/yjcpaj4/play_with_us/Main.java
@@ -148,8 +148,8 @@ public class Main {
     }
     
     public void launch() {
-        mWindow.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-        mWindow.setUndecorated(true);
+        //mWindow.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        //mWindow.setUndecorated(true);
         mWindow.setLocationRelativeTo(null);
         mWindow.setVisible(true);
         
@@ -158,8 +158,8 @@ public class Main {
     
     private class ResourceDownload extends GraphicLooper {
         
-        private final String DBX_CLIENT_ID = "play-with-us/v0.1";
-        private final String DBX_ACCESS_TOKEN = "O6KQjX3-8tAAAAAAAAAAFX5otGZsbsLIHma4OwgDnc1TOqUtSfssR7azAy8O9Jfb";
+        private final String DBX_CLIENT_ID = "play_with_us/v0.1";
+        private final String DBX_ACCESS_TOKEN = "BG8Lirg0MOAAAAAAAAAAG2rc4quBs-xL2yoFBPxrUdyembJNsZyBrrZ6v8JlmSg5";
         private final String DBX_RESOURCE_DIR = "/resources";
 
         private static final int PADDING = 25;
@@ -181,52 +181,65 @@ public class Main {
             }
         }
         
-        private boolean isValid() {
-            try {
-                File chksum = new File(RESOURCE_DIR, RESOURCE_CHECKSUM_FILE);
-
-                if (chksum.exists()) {
-                    chksum.delete();
-                }
-
-                mDropbox.download(DBX_RESOURCE_DIR + "/" + RESOURCE_CHECKSUM_FILE, chksum);
-
-                Map<String, String> m = new Gson().fromJson(FileUtil.getContents(chksum), Map.class);
-
-                boolean isValid = true;
-
-                for(String s : m.keySet()) {
-                    File res = new File(RESOURCE_DIR, s);
-
-                    if (res.exists()) { // 리소스가 이미 있고
-                        if (FileUtil.getChecksum(res).equals(m.get(s))) { // 체크섬 검사가 올바르면
-                            continue; // 통과
-                        }
-
-                        if (res.delete()) { // 파일이 있지만 존재한다면 삭제작업
-                            res.deleteOnExit();
-                        }
-                    }
-
-                    isValid = false;
-                }
-
-                return isValid;
-            }
-            catch(Exception e) {
-                return false;
-            }
-        }
-        
         @Override
         public void start() {
             super.start();
             
-            // 완료작업
-            Runnable r = new Runnable() {
+            mProgress = 0f;
+            mMessage = "게임에 필요한 리소스를 검사합니다.";
+            
+            // 다운로드 하는 작업을 별도의 스레드로 뺍니다.
+            new Thread() {
                 
                 @Override
                 public void run() {
+
+                    File f = new File(RESOURCE_DIR, RESOURCE_CHECKSUM_FILE);
+
+                    if (f.exists()) {
+                        f.delete();
+                    }
+                    
+                    final Map<String, String> m;
+
+                    try { // 체크섬 파일 다운받기
+                        mDropbox.download(DBX_RESOURCE_DIR + "/" + RESOURCE_CHECKSUM_FILE, f);
+
+                        m = new Gson().fromJson(FileUtil.getContents(f), Map.class);
+                    }
+                    catch(Exception e) {
+                        e.printStackTrace();
+                        return;
+                    }
+                    
+                    float n = 0;
+                    for(String s : m.keySet()) { // 체크섬 목록을 토대로 리소스의 유효성검사
+                        File res = new File(RESOURCE_DIR, s);
+                        
+                        
+                        if (res.exists()) { // 리소스가 이미 있고   
+                            if ( ! FileUtil.getChecksum(res).equals(m.get(s))) { // 체크섬 검사가 올바르지 않으면
+                                
+                                if (res.delete()) { // 리소스 제거
+                                    res.deleteOnExit();
+                                }
+                            }
+                        }
+                        
+                        if ( ! res.exists()) { // 위에서 리소스가 제거되었거나 없는 리소스라면 드롭박스에서 다운받기
+                            try {
+                                mDropbox.download(DBX_RESOURCE_DIR + "/" + s, res);
+                            }
+                            catch(Exception e) {
+                                e.printStackTrace();
+                                // TODO... 오류처리
+                            }
+                        } 
+                        
+                        mProgress = ++n / m.size();
+                        mMessage = String.format("게임에 필요한 리소스를 내려받고 있습니다. (%.1f%%)", mProgress * 100);
+                    }
+
                     mProgress = 1.0f;
                     mMessage = "게임에 필요한 리소스를 불러오는중 입니다.";
 
@@ -239,58 +252,11 @@ public class Main {
                         return;
                     }
 
-                    stop();
+                    ResourceDownload.this.stop();
                     
                     mLayer.remove(getComponent());
 
                     playVideo(VideoResource.MOV_INTRO);
-                }
-            };
-        
-            mMessage = "게임에 필요한 리소스를 검사합니다.";
-            
-            // 다운로드 하는 작업을 별도의 스레드로 뺍니다.
-            new Thread() {
-                
-                @Override
-                public void run() {
-                    
-                    // 그냥 아래에 다 포함시켜야할듯...
-                    if (isValid()) {
-                        r.run();
-                        return;
-                    }
-
-                    Map<String, String> m;
-
-                    try {
-                        m = new Gson().fromJson(FileUtil.getContents(new File(RESOURCE_DIR, RESOURCE_CHECKSUM_FILE)), Map.class);
-                    }
-                    catch(IOException e) {
-                        return;
-                    }
-                    
-                    float n = 0;
-                    for(String s : m.keySet()) {
-                        File res = new File(RESOURCE_DIR, s);
-
-                        if ( ! res.exists()) { // 리소스가 이미 있고
-                            try {
-                                mDropbox.download(DBX_RESOURCE_DIR + "/" + s, res);
-                                mProgress = ++n / m.size();
-                            }
-                            catch(Exception e) {
-                                // TODO... 오류
-                            }
-                        } 
-                        else {
-                            mProgress = ++n / m.size();
-                        }
-  
-                        mMessage = String.format("게임에 필요한 리소스를 내려받고 있습니다. (%.1f%%)", n * 100);
-                    }
-
-                    r.run();
                 }
             }.start();
         }
