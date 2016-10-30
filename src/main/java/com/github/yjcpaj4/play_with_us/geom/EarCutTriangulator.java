dc04ed5784b0190e8ef70f0cfee02f10ebfd0fd4
aw9223 <fukeric@gmail.com>
2016-10-30 15:39:37 +0900

    commit

--- src/main/java/com/github/yjcpaj4/play_with_us/geom/EarCutTriangulator.java
+++ src/main/java/com/github/yjcpaj4/play_with_us/geom/EarCutTriangulator.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.github.yjcpaj4.play_with_us.geom;
 
 import java.util.ArrayList;
